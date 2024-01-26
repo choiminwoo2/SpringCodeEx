3433cfe6c0f6180f86ebd58b21c7fd59b90f0d6f
wjrdjs005 <wjrdjs005@naver.com>
2024-01-26 21:56:48 +0900

    modify: 테스트코드 수정

--- RestWebServer/src/test/java/com/example/restwebserver/GrettingTest.java
+++ RestWebServer/src/test/java/com/example/restwebserver/GrettingTest.java
@@ -1,38 +1,35 @@
 package com.example.restwebserver;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class GrettingTest {
+
     @Autowired
     MockMvc mockMvc;
 
     @Test
-    void Greeting() throws Exception{
-        mockMvc.perform(get("/greeting"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hello, World"));
+    void Greeting() throws Exception {
+        mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
 
     }
+
     @Test
-    void greetingWithName() throws Exception{
-        mockMvc.perform(get("/greeting").param("name", "Spring"))
-                .andDo(print())
+    void greetingWithName() throws Exception {
+        mockMvc.perform(get("/greeting").param("name", "Spring")).andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hello, Spring"));
+                .andExpect(jsonPath("$.content").value("Hello, Spring!"));
 
     }
 
