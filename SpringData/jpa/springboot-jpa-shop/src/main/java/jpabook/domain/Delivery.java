d52ced6f1abbc95a3a58542a4fbca79e778be134
ruu <wjrdjs005@naver.com>
2024-03-18 10:09:52 +0900

    깃add: JPA연관관계 및ë  및 레포지토리 테스트 주문 관련 엔티티 관계 설정 멤버 저장 및 멤버 조회 테테스트

--- SpringData/jpa/springboot-jpa-shop/src/main/java/jpabook/domain/Delivery.java
+++ SpringData/jpa/springboot-jpa-shop/src/main/java/jpabook/domain/Delivery.java
@@ -1,31 +1,35 @@
 package jpabook.domain;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.OneToOne;
+import java.util.Arrays;
 import jpabook.domain.item.Address;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Getter
 @Setter
-public class Member {
+@Getter
+public class Delivery {
 
     @Id
     @GeneratedValue
-    @Column(name = "member_id")
+    @Column(name = "delivery_id")
     private Long id;
-    private String username;
+
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus deliveryStatus;
+
     @Embedded
     private Address address;
-
-    @OneToMany(mappedBy = "member")
-    private List<Order> orders = new ArrayList<>();
 }
