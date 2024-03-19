5e98799c5c9542571d0b81b4830437c04a1ff0a8
ruu <wjrdjs005@naver.com>
2024-03-19 22:22:49 +0900

    test: 레포지토리 테스트코드 변경 @RepositoryTest 어노테이션 추가 및 일괄 적용

--- SpringData/jpa/springboot-jpa-shop/src/test/java/jpabook/repository/RepositoryTest.java
+++ SpringData/jpa/springboot-jpa-shop/src/test/java/jpabook/repository/RepositoryTest.java
@@ -4,8 +4,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import javax.swing.Spring;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,9 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Repository;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Repository.class))
 @ActiveProfiles(value = "test")
 @AutoConfigureTestDatabase(replace = Replace.NONE)
